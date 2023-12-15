ee9732af1edadf9ebf49e2265ac84606c2902dc6
Abubakar Abubakar Yusif <abubakaryusuf951@gmail.com>
2023-12-15 17:58:12 +0800

    runner: enode: compose:
    runner: add param for help and init to install dependencies and build
    doker image
    enode: fix timeout function to wait for bootnodes to be available before
    proceeding to start normal nodes
    compose: add names for the containers for easy identification
    
    Signed-off-by: Abubakar Abubakar Yusif <abubakaryusuf951@gmail.com>

--- containers/data/scripts/enode.sh
+++ containers/data/scripts/enode.sh
@@ -6,14 +6,16 @@ declare ENODE0=""
 while [ -z "$ENODE0" ]; do
     ENODE0=$(curl -s -X POST --data '{"jsonrpc":"2.0","method":"admin_nodeInfo","params":[],"id":1}' http://127.0.0.1:8545 | jq -r '.result.enode')
     
-    if [ -z "$ENODE0" ]; then
-        echo "Enode is empty. Sleeping for $TIMEOUT seconds and retrying..."
+    if [ -z "$ENODE0" ] || [ -z "${ENODE0+x}" ] || [ "$ENODE0" = "null" ]; then
+        echo "Enode is either empty or null. Sleeping for $TIMEOUT seconds and retrying..."
         sleep $TIMEOUT
+    else
+        # Save ENODE0 to a file
+        echo "$ENODE0" > /opt/besu/data/genesis_enode
+        
+        # Run Besu command with --bootnodes
+        /opt/besu/bin/besu --bootnodes="$ENODE0" "$@"
+        echo "Enode is $ENODE0"
     fi
 done
 
-# Save ENODE0 to a file
-echo "$ENODE0" > /opt/besu/data/genesis_enode
-
-# Run Besu command with --bootnodes
-/opt/besu/bin/besu --bootnodes="$ENODE0" "$@"
